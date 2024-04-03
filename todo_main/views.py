2a5f92da6fa8c9596abe7614cf27766e43a0e59b
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-04-03 12:06:36 +0100

    Mark as done completed

--- todo_main/views.py
+++ todo_main/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render
 from todo.models import Task
 
 def home(request):
-    tasks = Task.objects.filter(is_completed=False)
+    tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+
+    completed_tasks = Task.objects.filter(is_completed=True)
+    print(completed_tasks)
     context = {
         'tasks': tasks,
+        'completed_tasks':completed_tasks
     }
     return render(request, 'home.html',context)
